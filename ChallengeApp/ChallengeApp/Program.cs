afb6397f0a48af5a9f16e8dc3864ca0e4bdbfb32
Jakub Ropski <jakub.ropski@gmail.com>
2023-07-27 12:38:29 +0200

    Zadanie domowe, lekcja 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string imie = "Alicja";
+var wiek = 27;
+bool kobieta = true;
+if (wiek < 30 && kobieta == true)
+    Console.WriteLine("Kobieta poniżej 30 lat");
+else if (wiek == 33 && imie == "Ewa")
+    Console.WriteLine("Ewa, lat 33");
+else if (wiek >= 30 && kobieta == true)
+    Console.WriteLine("Kobieta powyżej 30 lat");
+else if (wiek >= 18 && kobieta == false)
+    Console.WriteLine("Pełnoletni mężczyzna");
+else
+    Console.WriteLine("Niepełnoletni mężczyzna");
